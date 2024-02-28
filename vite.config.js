54a01bc5fb8a86a4ecc7918637dcf760924d08d0
samadhi1311 <69573759+samadhi1311@users.noreply.github.com>
2024-02-28 12:38:37 +0530

    Deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,19 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-	const config = {
-		plugins: [react()],
-		base: '/',
-	};
+export default defineConfig({
+	plugins: [react()],
+	base: '/Dev-Portfolio/',
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			onwarn(warning, defaultHandler) {
+				if (warning.code === 'SOURCEMAP_ERROR') {
+					return;
+				}
 
-	if (command !== 'serve') {
-		config.base = '/Dev-Portfolio/';
-	}
-
-	return config;
+				defaultHandler(warning);
+			},
+		},
+	},
 });
